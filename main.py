bba67e7eaecf44e6f3356522990f186245ae4367
ANDREW MICHAEL DE OLIVEIRA <amooliveira123@gmail.com>
2025-07-22 15:03:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ def salvar_analise(analise: AnaliseInput):
 
 if __name__ == "__main__":
     import uvicorn
-   CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
