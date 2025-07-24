c326c48b07ef49813141896026eae736ee7922f7
ANDREW MICHAEL DE OLIVEIRA <amooliveira123@gmail.com>
2025-07-24 14:33:09 -0300

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv()  # Carrega as variáveis do .env
@@ -10,6 +11,16 @@ def get_connection():
     
     print("[DEBUG] DATABASE_URL:", db_url)  # Log útil pra testar
 
-    return psycopg2.connect(db_url)
+    result = urlparse(db_url)
+
+    return psycopg2.connect(
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port,
+        sslmode="require"  # 👈 ESSENCIAL pro Railway
+    )
+
 
 
